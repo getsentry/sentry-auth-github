c478eac7bca7eb7bef2dd0a495afb4c01c74c1d9
David Cramer <dcramer@gmail.com>
2015-07-20 12:15:40 +0200

    Allow configuration of domain

--- sentry_auth_github/constants.py
+++ sentry_auth_github/constants.py
@@ -16,4 +16,4 @@ ERR_NO_ORG_ACCESS = 'You do not have access to the required GitHub organization.
 # we request repo as we share scopes with the other GitHub integration
 SCOPE = 'user:email,read:org,repo'
 
-DOMAIN = 'api.github.com'
+DOMAIN = getattr(settings, 'GITHUB_DOMAIN', 'api.github.com')
