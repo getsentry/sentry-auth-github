a454f0119d59bc0d6b66196e4585566f7d343707
David Cramer <dcramer@gmail.com>
2015-07-20 11:52:33 +0200

    Initial commit

---
+++ sentry_auth_github/constants.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, print_function
+
+from django.conf import settings
+
+
+AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
+
+ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+
+CLIENT_ID = getattr(settings, 'GITHUB_APP_ID', None)
+
+CLIENT_SECRET = getattr(settings, 'GITHUB_API_SECRET', None)
+
+ERR_NO_ORG_ACCESS = 'You do not have access to the required GitHub organization.'
+
+# we request repo as we share scopes with the other GitHub integration
+SCOPE = 'user:email,read:org,repo'
+
+DOMAIN = 'api.github.com'
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+sentry-auth-github
+==================
+
+:copyright: (c) 2015 Functional Software, Inc
+"""
+from setuptools import setup, find_packages
+
+
+tests_require = [
+    'pytest',
+    'mock',
+]
+
+install_requires = [
+]
+
+setup(
+    name='sentry-auth-github',
+    version='0.1.0',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='https://www.getsentry.com',
+    description='GitHub authentication provider for Sentry',
+    long_description=__doc__,
+    license='',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'tests': tests_require},
+    include_package_data=True,
+    entry_points={
+        'sentry.apps': [
+            'auth_github = sentry_auth_github',
+         ],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
