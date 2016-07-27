819951e5674eb901c5239fbf87d5f477b19d6a9b
Matt Robenolt <matt@ydekproductions.com>
2016-07-26 17:06:04 -0700

    Bind tests to sqlite

--- conftest.py
+++ conftest.py
@@ -5,6 +5,10 @@ import os.path
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
 
+# Run tests against sqlite for simplicity
+import os
+os.environ.setdefault('DB', 'sqlite')
+
 pytest_plugins = [
     'sentry.utils.pytest'
 ]
